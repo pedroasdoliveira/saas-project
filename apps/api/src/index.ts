0e8db128bb96c0909442285374a3a026fe8c3ba7
Pedro Oliveira <Pedroaolive@gmail.com>
2024-11-03 16:51:23 -0300

    config: config permissions

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,3 +1,5 @@
-import { ability } from '@tech/auth'
+import { defineAbilityFor } from '@tech/auth'
+
+const ability = defineAbilityFor({ role: 'ADMIN' })
 
 const userCanInviteSomeoneElse = ability.can('invite', 'User')
